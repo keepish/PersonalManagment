332604a6e4d1d4452dd66c88390815923f8bd857
Kolosov Vadim <157739955+keepish@users.noreply.github.com>
2025-06-15 17:31:14 +0300

    Добавьте файлы проекта.

---
+++ SeviceLayer/Context/AppDbContext.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SeviceLayer.Models;
+
+namespace SeviceLayer.Context;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Department> Departments { get; set; }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Position> Positions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=MSI\\MSSQLSERVER04;Initial Catalog=OrgDb;User ID=keepish;Password=12344321;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.ToTable("Department");
+
+            entity.Property(e => e.Name).HasMaxLength(150);
+        });
+
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Birthday).HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(150);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Patronymic).HasMaxLength(100);
+            entity.Property(e => e.Phone)
+                .HasMaxLength(11)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.Surname).HasMaxLength(100);
+
+            entity.HasOne(d => d.Department).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.DepartmentId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Employee_Department");
+
+            entity.HasOne(d => d.Position).WithMany(p => p.Employees)
+                .HasForeignKey(d => d.PositionId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Employee_Position");
+        });
+
+        modelBuilder.Entity<Position>(entity =>
+        {
+            entity.ToTable("Position");
+
+            entity.Property(e => e.Name).HasMaxLength(150);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ SeviceLayer/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace SeviceLayer.Models;
+
+public partial class Employee
+{
+    public int Id { get; set; }
+
+    public int DepartmentId { get; set; }
+
+    public int PositionId { get; set; }
+
+    public string Surname { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string? Patronymic { get; set; }
+
+    public DateTime? Birthday { get; set; }
+
+    public string Phone { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public virtual Department Department { get; set; } = null!;
+
+    public virtual Position Position { get; set; } = null!;
+}
